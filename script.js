5c4e8a64c1b24d63c224aeced38ad50dc808c4ba
huyphamsofteng <do.giahuy.pham@gmail.com>
2024-03-07 11:08:12 -0500

    Fix navbar

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const navButton = document.getElementsByClassName('nav-button')[0]
+const navBurger = document.getElementsByClassName('nav-burger')[0]
+const navLink = document.getElementsByClassName('nav-link')[0]
+const logo = document.getElementsByClassName('logo-letter')
 
-navButton.addEventListener('mouseover',() =>{
-    navButton.innerHTML = "X";
+navBurger.addEventListener('click',()=>{
+    navLink.classList.toggle('enable');
 })
 
-navButton.addEventListener('mouseleave',() =>{
-    navButton.innerHTML = "=";
-})
+color_arr =['#FA7070','#FFB38E','#FF8E8F','#E178C5','#F7418F','#FC819E','#FEC7B4','#FF8080','#E6A4B4','#EC7272']
+for(let i  = 0; i < 10; i++ ){
+    logo[i].addEventListener('mouseover', () =>{
+        logo[i].style.fontSize = '33px';
+        logo[i].style.color = color_arr[i];
+    })
+    logo[i].addEventListener('mouseleave', () =>{
+        logo[i].style.fontSize = '23px';
+        logo[i].style.color = 'black';
+    })
+}
