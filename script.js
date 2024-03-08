e8f89d613987bdc554b103fa6917af04ba25f3a9
huyphamsofteng <do.giahuy.pham@gmail.com>
2024-03-07 21:46:24 -0500

    Finish banner and modify navbar

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
-const navBurger = document.getElementsByClassName('nav-burger')[0]
-const navLink = document.getElementsByClassName('nav-link')[0]
-const logo = document.getElementsByClassName('logo-letter')
+const navBurger = document.querySelector('.nav-burger');
+const navLink = document.querySelector('.nav-link');
+const logo = document.getElementsByClassName('logo-letter');
+const bannerText = document.querySelectorAll('.banner-span');
+const navIcon = document.getElementsByClassName('line');
+const profileImg = document.querySelector('.profile');
+const hello = document.querySelector('.hello');
 
-navBurger.addEventListener('click',()=>{
-    navLink.classList.toggle('enable');
-})
-
-color_arr =['#FA7070','#FFB38E','#FF8E8F','#E178C5','#F7418F','#FC819E','#FEC7B4','#FF8080','#E6A4B4','#EC7272']
-for(let i  = 0; i < 10; i++ ){
-    logo[i].addEventListener('mouseover', () =>{
-        logo[i].style.fontSize = '33px';
-        logo[i].style.color = color_arr[i];
+const navBar = () => {
+    navBurger.addEventListener('click',()=>{
+        navLink.classList.toggle('toggle');   
+        navLink.classList.remove('hidden');
+        navIcon[0].classList.toggle('nav-burger-toggle');
+        navIcon[1].classList.toggle('nav-burger-toggle-1');
+        navIcon[2].classList.toggle('nav-burger-toggle-2');
     })
-    logo[i].addEventListener('mouseleave', () =>{
-        logo[i].style.fontSize = '23px';
-        logo[i].style.color = 'black';
+    
+    navLink.addEventListener('mouseleave',()=>{
+        navLink.classList.add('hidden');
+        navLink.classList.remove('toggle'); 
+        navIcon[0].classList.remove('nav-burger-toggle');
+        navIcon[1].classList.remove('nav-burger-toggle-1');
+        navIcon[2].classList.remove('nav-burger-toggle-2');
     })
 }
+
+const banner = () =>{
+    let color_arr = ['#9A031E','#E36414','#FB8B24']
+    for (let i = 0; i <= 9; i++ ){
+        bannerText[i].addEventListener('mouseover', () =>{
+            bannerText[i].style.color = color_arr[0];
+            bannerText[i+1].style.color = color_arr[1];
+            bannerText[i-1].style.color = color_arr[2];
+        })
+        bannerText[i].addEventListener('mouseleave', () =>{
+            bannerText[i].style.color = 'black';
+            bannerText[i+1].style.color = 'black';
+            bannerText[i-1].style.color = 'black';
+        })
+    }
+}
+
+const profileToggle = () => {
+    profileImg.addEventListener('mouseover',() =>{
+        hello.style.backgroundColor = '#FCDC2A';
+    })
+    profileImg.addEventListener('mouseleave',() =>{
+        hello.style.backgroundColor = 'white';
+    })
+}
+profileToggle();
+navBar();
+logoColor();
+banner();
+
+
+
+
+
+
+
+
+
