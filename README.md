0360dab95943c2a43390efa90af1a6474fc8fc60
huyphamsofteng <do.giahuy.pham@gmail.com>
2024-03-28 21:47:14 -0400

    Fix bugs

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# This is a Web Portfolio
+[![LinkedIn][linkedin-shield]][linkedin-url] [![web][web-shield]][web-url] 
+[![web][resume-shield]][resume-url]
+
+<h1>Web Portfolio Project</h1>
+A little introduction about me.
+<h2>Table of Contents</h2>
+
+1. Introduction
+2. Installation
+3. Breakdown
+
+<h2>Introduction</h2>
+
+The project uses HTML, CSS and JavaScript to structure, style and interactive a web app. Being hosted in GitHub Page and using <a href='www.namecheap.com'>Namecheap</a> as DNS Provider. 
+
+<h2>Installation</h2>
+This is a static website therefore no need additional installation.
+
+
+
+
+[linkedin-url]: https://www.linkedin.com/in/harryph8605/
+[linkedin-shield]: https://img.shields.io/badge/LinkedIn-blue?style=for-the-badge
+[web-url]: https://devhuypham.me
+[web-shield]:https://img.shields.io/badge/Harry_Pham-F96167?style=for-the-badge
+[resume-url]: https://drive.google.com/file/d/1NxrqI-Wi9R3qad9Qz-mI5z1zJ5Yu6m_a/view?usp=sharing
+[resume-shield]: https://img.shields.io/badge/Resume-F9E795?style=for-the-badge
+
 
--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 let left = document.querySelector(".left")
 let right = document.querySelector(".right")
 let slider = document.querySelector(".slider")
+let projectsItem = document.querySelectorAll(".projects-order")
+
 const intersectionObs = document.querySelectorAll('.transitioning');
 
 let pContainer = document.querySelector(".projects-container")
 let navbar = document.querySelector(".navbar")
 
-
 let width = slider.offsetWidth
 let index = 0
+
 window.addEventListener("resize", function () {
     width = project.offsetWidth;
 });
 
 right.addEventListener("click", function () {
-    index = 1;
-    pContainer.style.transform = `translateX(-${index * width}px)`;
+    index++;
+    if ( index <= 1 ){
+        pContainer.style.transform = `translateX(-${index * width}px)`;
+    }
 });
 
 left.addEventListener("click", function () {
-    index = 0;
-    pContainer.style.transform = `translateX(-${index * width}px)`;
+    index--;
+    if(index >= 0){
+        pContainer.style.transform = `translateX(-${index * width}px)`;
+    }
 });
 
 const observer = new IntersectionObserver(entries => {
